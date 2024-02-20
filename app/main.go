792cebef07b09a8bb3e67690f4b0962bf87d5eaa
FacundoC <facundocabrera2898@gmail.com>
2024-02-20 12:35:11 -0300

    Test Push

--- app/main.go
+++ app/main.go
@@ -7,11 +7,10 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello everybody, you accesed %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello everybody, you %s!", r.URL.Path[1:])
 }
 
 func main() {
 	http.HandleFunc("/", home)
-
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
